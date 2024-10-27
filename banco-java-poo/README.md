3521cfa7bbbf71f14e96b3b100df23706c17f12c
Thiago <thiagoesmael19@gmail.com>
2024-10-27 18:17:53 -0300

    Feat: Adicionando as principais Classes e iniciando o processo de abstração do Domínio Bancário

---
+++ banco-java-poo/README.md
@@ -0,0 +1,1 @@
+Inicio abstração Banco
---
+++ banco-java-poo/src/ContaPoupança.java
@@ -0,0 +1,17 @@
+public class ContaPoupança {
+    public int agencia;
+    public int numeroConta;
+    public double saldo;
+
+    public void sacar(){
+
+    }
+
+    public void depositar(){
+        
+    }
+
+    public void transferir(){
+        
+    }
+}
